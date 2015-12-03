d90cac1d24e07b637c67de0088192e9453ea87e1
pr1ntf <trentnthompson@gmail.com>
2015-12-03 07:31:06 -0700

    extract all drivers for good measure

--- extract.sh
+++ extract.sh
@@ -4,10 +4,10 @@
 
 folder=win2k8
 iso=Win2k8R2.iso
-drivers=virtio-win-0.1.96.iso
+drivers=virtio-win-0.1-94.iso
 
 mkdir -p ${folder}/virtio
 
 7z x ${iso} -o${folder}
 
-tar xf $drivers --include="NetKVM/2k12R2/amd64/*" -C ${folder}/virtio
+tar xf $drivers -C ${folder}/virtio
