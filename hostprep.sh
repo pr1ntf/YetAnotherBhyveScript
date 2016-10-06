7bf24337fa2ea2778de4c9400d4eadef939db474
pr1ntf <trentnthompson@gmail.com>
2016-10-05 21:45:09 -0600

    load tap and bridge mods

--- hostprep.sh
+++ hostprep.sh
@@ -2,11 +2,13 @@
 
 # Prepare host for running bhyve (v0.2)
 
-iface=igb0
+iface=em0
 bridge=bridge0
 
 # Load kernel module
 kldload vmm
+kldload if_tap
+kldload if_bridge
 
 # Set sysctl
 sysctl net.link.tap.up_on_open=1
